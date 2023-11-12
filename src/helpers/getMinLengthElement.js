3472a2df35007625e769a43db8127460250a5f8e
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-11-13 00:26:30 +0200

    fix getMinLengthElement

--- src/helpers/getMinLengthElement.js
+++ src/helpers/getMinLengthElement.js
@@ -1,7 +1,5 @@
 const getMinLengthElement = (array) => {
-  return array.reduce((acc, elem) =>
-    acc.length > elem.length ? (acc = elem) : acc,
-  );
+  return array.reduce((acc, elem) => (acc.length > elem.length ? elem : acc));
 };
 
 export default getMinLengthElement;
